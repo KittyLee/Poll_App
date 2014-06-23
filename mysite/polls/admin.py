b2b3def9aca73453d8addd9a2a2e22859db77baf
KittyLee <katherine@codeforprogress.org>
2014-06-23 19:44:20 -0400

    many views and voting things

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from polls.models import Poll
+from polls.models import Choice, Poll 
+
 
 # Register your models here.
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
 
-admin.site.register(Poll)
+
+class PollAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, 		{'fields': ['question']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+]
+
+admin.site.register(Poll, PollAdmin)
+list_display = ('question', 'pub_date', 'was_published_recently')
+list_filter = ['pub_date']
+search_fields = ['question']
+inlines = ['ChoiceInline']
+
+
